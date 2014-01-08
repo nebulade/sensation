65994ec92e24b19378c23a86c34059fb3874dc74
Johannes Zellner <webmaster@nebulon.de>
2014-01-08 13:59:47 -0800

    Add publish subcommand to POST to a server

--- bin/sensation.js
+++ bin/sensation.js
@@ -2,7 +2,17 @@
 
 'use strict';
 
-var Provider = require('../src/index.js');
+var Provider = require('../src/index.js'),
+    request = require('superagent'),
+    debug = require('debug'),
+    p = require('commander');
+
+p.version('0.1.0')
+    .usage('<subcommand> [options]')
+    .option('-i --interval <seconds>', 'Define the value gather interval. [1 second]\n')
+    .option('print', 'Print the sensor value on the terminal. [default]')
+    .option('publish <url>', 'Publish (POST) the sensor value to a server.')
+    .parse(process.argv);
 
 var provider = new Provider();
 
@@ -11,11 +21,20 @@ provider.on('error', function (error) {
 });
 
 provider.on('temperature', function (temperature) {
-    console.log('Temperature:', temperature);
+    var that = this;
+
+    if (p.publish) {
+        request.post(p.publish).send({ temperature: temperature }).end(function (error, result) {
+            if (error || !result.ok) {
+                console.log('Error publishing sensor value.', error || result);
+                return;
+            }
+
+            console.log('Temperature %s pushed to %s.', temperature, p.publish);
+        });
+    } else {
+        console.log('Temperature:', temperature);
+    }
 });
 
-provider.start();
-
-setTimeout(function () {
-    provider.stop();
-}, 5000);
+provider.start(p.interval || 1000);
